776983f98ad8fa85678d4366a5c4fba2c6ff72b5
Diana Majolli Andre <dianamajolli@hotmail.com>
2024-02-14 17:23:38 -0500

    Starting Dashboard page

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dashboard() {
+  
+  return (
+    <div>
+      <h1>Manager's Dashboard</h1>
+      {/* Implement dashboard widgets and features */}
+    </div>
+  );
+}
+
+export default Dashboard;
