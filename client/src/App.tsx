01acc2973b5b7d1496729f42084141a4a19593d3
Diana Majolli Andre <dianamajolli@hotmail.com>
2024-03-09 19:01:51 -0500

    Protecting private routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,14 +8,14 @@ import PerformancePage from './pages/PerformancePage';
 import TrainingPage from './pages/TrainingPage';
 import CompensationPage from './pages/CompensationPage';
 import RecruitmentPage from './pages/RecruitmentPage';
-
+import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
     return (
         <Provider store={store}>
             <Router>
                 <Routes>
-                    <Route path="/" element={<Homepage />} />
+                    <Route path="/" element={<ProtectedRoute><Homepage /></ProtectedRoute>} />
                     <Route path="/Login" element={<LoginPage />} />
                     <Route path="/Performance" element={<PerformancePage />} />
                     <Route path="/Training" element={<TrainingPage />} />
