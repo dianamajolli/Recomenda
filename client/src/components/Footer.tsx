310cf029fc587e8af649ce91ce6c135f5c08d5c4
Diana Majolli Andre <dianamajolli@hotmail.com>
2024-03-17 14:44:40 -0400

    Updated css

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,12 +5,18 @@ const Footer = () => {
   
 
   return (
-    <footer className="w-full mt-32 bg-[#7724a7] text-white text-center py-4 [font-family:'Inria_Serif',Helvetica]">
+    
+      <footer className="w-full mt-8 bg-[#7724a7] text-white text-center py-4 [font-family:'Inria_Serif',Helvetica] ">
+
         <p className="font-normal text-sm">
         © Recomenda 2024 | Privacy Policy | Terms of Use
         </p>
       </footer>
+      
   );
 }
 
 export default Footer;
+
+
+
